a393f31047cd8e6ae605e74a574b63746f37c60a
awwa-y <3358735079@qq.com>
2025-11-29 19:42:48 +0800

    Update global.h

--- frontend/lifchatc_new/global.h
+++ frontend/lifchatc_new/global.h
@@ -9,10 +9,11 @@
 #include <QByteArray>
 #include <qdebug>
 #include <QNetworkReply>
+#include <QJsonObject>
 extern std::function<void(QWidget*)> repolish;
 
 enum ReqId{
-    ID_GET_VARIFY_CODE=1001,//获取验证码
+    ID_GET_VERIFY_CODE=1001,//获取验证码
     ID_REG_USER=1002,//注册用户
 
 };
