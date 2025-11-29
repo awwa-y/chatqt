95c591865b689b381228692990232ba72d05f5be
awwa-y <3358375079@qq.com>
2025-11-29 17:42:15 +0800

    重构: 分离前后端代码结构
    - 后端代码移动到 backend/src/
    - 前端代码移动到 frontend/src/
    - 添加项目文档和说明
    - 更新 .gitignore 忽略规则

---
+++ frontend/src/qqs/registerdialog.h
@@ -0,0 +1,32 @@
+#ifndef REGISTERDIALOG_H
+#define REGISTERDIALOG_H
+
+#include <QDialog>
+#include "global.h"
+namespace Ui {
+class registerDialog;
+}
+
+class registerDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit registerDialog(QWidget *parent = nullptr);
+    ~registerDialog();
+
+signals:
+    void backToLogin(); // 返回到登录界面的信号
+
+private slots:
+    void on_get_code_clicked();//获取验证码判断合法性
+    void slot_reg_mod_finish(ReqId id,QString res,ErrorCodes err);
+private:
+    void showTip(QString str,bool b_ok);
+    void initHttpHandlers();
+private:
+    Ui::registerDialog *ui;
+    QMap<ReqId,std::function<void(const QJsonObject&)>>_handlers;
+};
+
+#endif // REGISTERDIALOG_H
