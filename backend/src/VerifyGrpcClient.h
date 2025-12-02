6703cd8b6de68c04a8f42c7210c5e36fa1811a6d
awwa-y <3358735079@qq.com>
2025-12-02 22:33:07 +0800

    Add GetVarifyCode method to VerifyGrpcClient
    Refactor VerifyGrpcClient to include GetVarifyCode method and handle RPC response.

--- backend/src/VerifyGrpcClient.h
+++ backend/src/VerifyGrpcClient.h
@@ -4,6 +4,7 @@
 #include "const.h"
 #include "Singleton.h"
 #include "message.pb.h"
+
 using grpc::Channel;
 using grpc::Status;
 using grpc::ClientContext;
@@ -13,12 +14,30 @@ using message::GetVarifyRsp;
 using message::VarifyService;
 
 
-class VerifyGrpcClient
+class VerifyGrpcClient:public Singleton<VerifyGrpcClient>
 {
-	friend class Singleton < VerifyGrpcClient>;
+	friend class Singleton <VerifyGrpcClient>;
 public:
-	GetVarifyRsp GetVarifyReq(std::string email) {
+	GetVarifyRsp GetVarifyCode(std::string email) {
 		ClientContext context;
+		GetVarifyRsp reply;
+		GetVarifyReq request;
+		request.set_email(email);
+		Status status = stub_->GetVarifyCode(&context,request,&reply);
+		if (status.ok()) {
+			return reply;
+		}
+		else {
+			reply.set_error(ErrorCodes::RPCFailed);
+			return reply;
+		}
 	}
+private:
+	VerifyGrpcClient() {
+		std::shared_ptr<Channel> channel = grpc::CreateChannel("0.0.0.0:50051", 
+			grpc::InsecureChannelCredentials());
+		stub_ = VarifyService::NewStub(channel);
+	}
+	std::unique_ptr<VarifyService::Stub> stub_;
 };
 
